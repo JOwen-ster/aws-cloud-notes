ae8cc34badc5ff15c4c1ce6dc85947fe62c4a3f3
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2026-03-24 09:36:58 -0700

    Update provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+
+Amplify.configure(outputs);
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
